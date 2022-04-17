76f4fac561d2c49bc189689cae8311962c2c41c0
unknown <ahammedrabby881@gmail.com>
2022-04-17 01:51:45 -0700

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css'
+const Footer = () => {
+    return (
+        <div>
+           <h6 className="footer">copy right 2022-2023 @shakhor ariyan</h6> 
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/nCC4KKq/bg-1.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/2NyKZVy/bg-2.webp",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/jr6g1cn/bg-3.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/4ghqzc7/bg-4.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/RCG8Jvb/bg-5.jpg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/f1pXhhC/bg-6.jpg",
+    link: "http://lama.dev",
+  },
+];
